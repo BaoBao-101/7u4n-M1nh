14fd89594eac569ad8297211bfc224634c976152
7u4n M1nh <tuanminh261204@gmail.com>
2025-03-11 08:24:55 +0700

    Create dashboard.php

---
+++ 16_3/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+    //code phan game
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+</head>
+<body>
+    <h1>Welcome, <?php echo $_SESSION["user"]; ?>!</h1>
+    <h2>Site nay de launch tro choi</h2>
+</body>
+</html>
